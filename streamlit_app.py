19ae2116f179c3a4b0c07f185a2fd303e661e543
1013dudal-web <1013dudal@gmail.com>
2025-08-19 11:17:42 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 김영미 소개하기")
+st.write("안녕하세요. 저는 수학교사입니다. 수학을 AI로 즐겁게 배워봐요")
